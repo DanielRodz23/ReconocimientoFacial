2465cec6131c4b7f3a7917fe973d175ac7994a1c
Daniel Andres Rodriguez Guerrero <201G0282@rcarbonifera.tecnm.mx>
2023-11-03 20:36:52 -0600

    Equis

--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -1,17 +1,26 @@
 import cv2
 
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 #face_cascade = cv2.CascadeClassifier('banana_classifier.xml')
 
 
 cap = cv2.VideoCapture(0)
 
+print(cap)
+
 while True:
     _, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    print()
+    print(faces)
+    print()
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0 ,255), 3)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0 ,255), 2)
     cv2.imshow('img',img)
     k = cv2.waitKey(30)
     if k==27:
